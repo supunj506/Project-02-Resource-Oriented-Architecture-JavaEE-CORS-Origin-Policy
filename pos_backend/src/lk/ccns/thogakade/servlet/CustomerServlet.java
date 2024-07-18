ce2ccbda2de256275e8ecb36529e443c6d1c28de
supunj506 <supunj506@gmail.com>
2024-07-18 16:11:05 +0530

    forntend update

---
+++ pos_backend/src/lk/ccns/thogakade/servlet/CustomerServlet.java
@@ -0,0 +1,16 @@
+/*
+ * @author : xCODE
+ * Project : Project02
+ * Date    : 7/18/2024 (Thursday)
+ * Time    : 4:07 PM
+ * For GDSE course of IJSE institute.
+ */
+
+package lk.ccns.thogakade.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+
+}
